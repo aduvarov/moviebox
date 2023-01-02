eda9e7a792e344152e21f225f0a5294a873d0fa8
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2023-01-02 20:36:29 +0600

    update: lesson-19

---
+++ routes/movie-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getMovies,
+    getMovie,
+    deleteMovie,
+    addMovie,
+    updateMovie,
+} = require('../controllers/movie-controller');
+
+router.get('/movies', getMovies);
+router.get('/movies/:id', getMovie);
+router.delete('/movies/:id', deleteMovie);
+router.post('/movies/', addMovie);
+router.patch('/movies/:id', updateMovie);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Movie = require('./models/movie');
-
+// const Movie = require('./models/movie');
+const movieRoutes = require('./routes/movie-routes');
 const PORT = 3000;
 const URL = 'mongodb://localhost:27017/moviebox';
 
 const app = express();
 app.use(express.json());
+app.use(movieRoutes);
 mongoose.set('strictQuery', false);
 
 mongoose
@@ -16,51 +17,4 @@ mongoose
 
 app.listen(PORT, error => {
     error ? console.error : console.log(`Application is running on port ${PORT}`);
-});
-
-const handleError = (res, error) => {
-    res.status(500).json({ error });
-};
-
-app.get('/movies', (req, res) => {
-    Movie.find()
-        .sort({ rating: 1 })
-        .then(movies => {
-            res.status(200).json(movies);
-        })
-        .catch(() => handleError(res, 'Something wrong'));
-});
-
-app.get('/movies/:id', (req, res) => {
-    Movie.findById({ _id: ObjectId(req.params.id) })
-        .then(movie => {
-            res.status(200).json(movie);
-        })
-        .catch(() => handleError(res, 'Something wrong'));
-});
-
-app.delete('/movies/:id', (req, res) => {
-    Movie.findByIdAndDelete(req.params.id)
-        .then(result => {
-            res.status(200).json(result);
-        })
-        .catch(() => handleError(res, 'Something wrong'));
-});
-
-app.post('/movies/', (req, res) => {
-    const movie = new Movie(req.body);
-    movie
-        .save()
-        .then(result => {
-            res.status(201).json(result);
-        })
-        .catch(() => handleError(res, 'Something wrong'));
-});
-
-app.patch('/movies/:id', (req, res) => {
-    Movie.findByIdAndUpdate(req.params.id, req.body)
-        .then(result => {
-            res.status(200).json(result);
-        })
-        .catch(() => handleError(res, 'Something wrong'));
 });
